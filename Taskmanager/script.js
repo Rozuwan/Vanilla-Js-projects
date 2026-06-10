364323cbec171a65dfd57b60a7c234f81e6d83ab
Roshan <pahariroshan55@gmail.com>
2026-06-10 15:51:16 +0545

    build the task manager

--- Taskmanager/script.js
+++ Taskmanager/script.js
@@ -12,36 +12,25 @@ addBtn.addEventListener("click", () => {
 
   const task = document.createElement("div");
   task.classList.add("task");
-
   const label = document.createElement("label");
-
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-
   checkbox.addEventListener("change", () => {
     label.classList.toggle("completed", checkbox.checked);
   });
-
   label.appendChild(checkbox);
   label.append(taskText);
-
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "×";
   deleteBtn.classList.add("btn");
-
   deleteBtn.addEventListener("click", () => {
     task.remove();
-
     totalTask--;
     span.textContent = totalTask;
   });
-
   task.append(label, deleteBtn);
-
   taskContainer.append(task);
-
   totalTask++;
   span.textContent = totalTask;
-
   textInput.value = "";
 });
